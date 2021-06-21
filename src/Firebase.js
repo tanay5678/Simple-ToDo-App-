ea28624c3942c7e2c97fda356a77b50acaa67770
Tanay Kale <46751294+tanay5678@users.noreply.github.com>
2021-06-21 21:44:34 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase'
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDdpvRb-5Ep7T9gslvj1spQVevXBj9U-uY",
-    authDomain: "todo-80d71.firebaseapp.com",
-    projectId: "todo-80d71",
-    storageBucket: "todo-80d71.appspot.com",
-    messagingSenderId: "176254510912",
-    appId: "1:176254510912:web:45065e7a931a59f75afca5",
-    measurementId: "G-P4HSTT980F"
-});
+//Here you need to put your Firebase Application Config information in order to connect to Firebase tools
 
 const db = firebaseApp.firestore();
 
